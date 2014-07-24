ab5d66869cb16205afd92c8087a25c73947c5def
John Doty <jrhdoty@gmail.com>
2014-07-23 19:11:34 -0700

    make sure server connected to socket.io is listening

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express  = require('express');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
+// app.use(express.static(__dirname + '/node_modules'));
 
 app.get('/', function(req, res){
   res.sendfile('public/angular/index.html');
 });
 
-var server = app.listen(3000, function(){
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+var server = http.listen(3000, function(){
   console.log('listening on 3000');
 });
